f60e9fe7d51fe6ac481cb4a02d3e8178650e24db
nano <nano@arch.localdomain>
2021-04-20 05:53:29 +0000

    Avances del Trabajo Practico n3 echos en clase

---
+++ TP3_RodriguezMariano/src/main/java/ar/edu/unju/edm/model/Cliente.java
@@ -0,0 +1,99 @@
+package ar.edu.unju.edm.model;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cliente {
+	@DateTimeFormat(pattern = "yyyy-MM-dd") 
+private LocalDate fechaNacimiento, fechaUltimaCompra;
+	private int nroDocumento, codigoAreaTelefono, numTelefono, edad;
+	private String tipoDocumento, email, nombreApellido, password;
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public LocalDate getFechaUltimaCompra() {
+		return fechaUltimaCompra;
+	}
+
+	public void setFechaUltimaCompra(LocalDate fechaUltimaCompra) {
+		this.fechaUltimaCompra = fechaUltimaCompra;
+	}
+
+	public int getNroDocumento() {
+		return nroDocumento;
+	}
+
+	public void setNroDocumento(int nroDocumento) {
+		this.nroDocumento = nroDocumento;
+	}
+
+	public int getCodigoAreaTelefono() {
+		return codigoAreaTelefono;
+	}
+
+	public void setCodigoAreaTelefono(int codigoAreaTelefono) {
+		this.codigoAreaTelefono = codigoAreaTelefono;
+	}
+
+	public int getNumTelefono() {
+		return numTelefono;
+	}
+
+	public void setNumTelefono(int numTelefono) {
+		this.numTelefono = numTelefono;
+	}
+
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNombreApellido() {
+		return nombreApellido;
+	}
+
+	public void setNombreApellido(String nombreApellido) {
+		this.nombreApellido = nombreApellido;
+	}
+	
+}
---
+++ TP3_RodriguezMariano/src/main/java/ar/edu/unju/edm/service/imp/OtroImp.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.edm.service.imp;
+
+import java.util.List;
+
+import ar.edu.unju.edm.model.Cliente;
+import ar.edu.unju.edm.service.IClienteService;
+//@Service
+public class OtroImp implements IClienteService{
+
+	@Override
+	public void guardarCliente(Cliente unCliente) {
+		// TODO Auto-generated method stub
+		//aqui podriamos guardar los datos en una base de datos....
+	}
+
+	@Override
+	public Cliente crearCliente() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> obtenerTodosClientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
